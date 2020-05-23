4c7dfcf47202f82f0349764a801f767092c6e33d
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-05-23 11:14:57 +0300

    Add demo LinkedList, HashSet, Stack!

---
+++ Module 4 - BASD/LinearDataStructure/D03-Stack/ArrayStack.cs
@@ -0,0 +1,47 @@
+﻿namespace D03_Stack
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ArrayStack<T>
+    {
+        private readonly List<T> data;
+        public ArrayStack()
+        {
+            this.data = new List<T>();
+        }
+
+        public int Count { get => data.Count; }
+
+        public void Push(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Pop()
+        {
+            if (data.Any())
+            {
+                T num = this.data[data.Count - 1];
+                this.data.RemoveAt(data.Count - 1);
+                return num;
+            }
+            throw new Exception("Stack is empty!");
+        }
+
+        public T Peek()
+        {
+            if (data.Any())
+            {
+                return this.data[data.Count - 1];
+            }
+            throw new Exception("Stack is empty!");
+        }
+
+        public T[] ToArray()
+        {
+            return data.ToArray();
+        }
+    }
+}
---
+++ Module 4 - BASD/LinearDataStructure/D03-Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace D03_Stack
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] nums = new int[] { 1, 2, 3 };
+
+            Stack<int> stack = new Stack<int>(nums);
+
+            //ArrayStack stack1 = new ArrayStack();
+            //stack.Push(12);
+            //stack.Push(15);
+            //Console.WriteLine(stack.Peek());
+
+            while (stack.Any())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
