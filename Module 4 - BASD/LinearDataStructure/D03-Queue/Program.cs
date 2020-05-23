4c4fa353e74e6b35d6021ee533825d8e0d2b18fa
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-05-23 11:25:20 +0300

    Add demo Queue!

---
+++ Module 4 - BASD/LinearDataStructure/D03-Queue/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace D03_Queue
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5 };
+
+            Queue<int> q = new Queue<int>(nums);
+            q.Enqueue(10);
+            Console.WriteLine(q.Peek());
+
+            while (q.Any())
+            {
+                Console.WriteLine(q.Dequeue());
+            }
+        }
+    }
+}
