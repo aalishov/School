8c70b66962c477176b54648737d7abd657ee40e9
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-05-31 12:16:25 +0300

    Add new projects (Ex04) and problem descriptions!

---
+++ Module 4 - BASD/LinearDataStructure/Ex04-P01-BasicStackOperations/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Ex04_P01_BasicStackOperations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        private static Stack<int> nums;
+        public static void Main()
+        {
+            int[] input = ReadNumsArr();
+
+            int stackLength = input[0];
+            int popNum = input[1];
+            int find = input[2];
+
+            nums = new Stack<int>(ReadNumsArr());
+
+            for (int i = 0; i < popNum; i++)
+            {
+                nums.Pop();
+            }
+
+            PrintResult(find);
+        }
+
+        private static void PrintResult(int find)
+        {
+            if (nums.Contains(find))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                int minElement = nums.Min();
+                Console.WriteLine(minElement);
+            }
+        }
+
+        private static int[] ReadNumsArr()
+        {
+            return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ Module 4 - BASD/LinearDataStructure/Ex04-P05-FashionBoutique/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Ex04_P05_FashionBoutique
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Stack<int> box = new Stack<int>(ReadArrNums());
+
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int countRack = 1;
+            int currentRackValue = 0;
+
+            while (box.Any())
+            {
+                int clothes = box.Peek();
+
+                if (rackCapacity>=clothes+currentRackValue)
+                {
+                    currentRackValue += clothes;
+                    box.Pop();
+                }
+                else
+                {
+                    countRack++;
+                    currentRackValue = 0;
+                }
+            }
+
+            Console.WriteLine(countRack);
+        }
+
+        private static int[] ReadArrNums()
+        {
+            return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        }
+    }
+}
