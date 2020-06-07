f99acf2be9201ca8955862a1bb4d49cca1cd8f60
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-06-07 12:41:27 +0300

    Add ASD-ExamPreparation.sln with several projects!

---
+++ Module 4 - BASD/ASD-ExamPreparation/Ex01-FightingArena/Arena.cs
@@ -0,0 +1,48 @@
+﻿namespace Ex01_FightingArena
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Arena
+    {
+        private readonly List<Gladiator> gladiators;
+
+        public Arena(string name)
+        {
+            this.Name = name;
+            this.gladiators = new List<Gladiator>();
+        }
+
+        public string Name { get; set; }
+
+        public int Count { get { return gladiators.Count; } }
+        public void Add(Gladiator gladiator)
+        {
+            this.gladiators.Add(gladiator);
+        }
+        public void Remove(string name)
+        {
+            Gladiator gladiator = gladiators.FirstOrDefault(x => x.Name == name);
+            this.gladiators.Remove(gladiator);
+        }
+
+        public Gladiator GetGladitorWithHighestStatPower()
+        {
+            return gladiators.OrderByDescending(x => x.GetStatPower()).First();
+        }
+
+        public Gladiator GetGladitorWithHighestWeaponPower()
+        {
+            return gladiators.OrderByDescending(x => x.GetWeaponPower()).First();
+        }
+
+        public Gladiator GetGladitorWithHighestTotalPower()
+        {
+            return gladiators.OrderByDescending(x => x.GetTotalaPower()).First();
+        }
+
+        public override string ToString()
+        {
+            return $"[{this.Name}] - [{this.Count}] gladiators are participating.";
+        }
+    }
+}
---
+++ Module 4 - BASD/ASD-ExamPreparation/Ex07-Pharmacy/Medicine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex07_Pharmacy
+{
+    public class Medicine
+    {
+        private double price;
+
+        public Medicine(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name { get; set; }
+        public double Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Invalid price");
+                }
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Medicine: {this.Name} with price {this.Price:f2}";
+        }
+    }
+}
