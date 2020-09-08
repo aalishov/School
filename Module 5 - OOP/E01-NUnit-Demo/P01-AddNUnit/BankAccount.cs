309d2680a35e5cb7a0790f012a49e373409081ad
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-09-08 12:47:04 +0300

    Start module OOP!

---
+++ Module 5 - OOP/E01-NUnit-Demo/P01-AddNUnit/BankAccount.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+namespace P01_AddNUnit
+{
+    public class BankAccount
+    {
+        private decimal amount;
+
+        public BankAccount(decimal initialBalance)
+        {
+            this.Amount = initialBalance;
+        }
+        public decimal Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Value must be positive!");
+                }
+                this.amount = value;
+            }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Value must be positive!");
+            }
+            this.Amount += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount<0)
+            {
+                throw new ArgumentException("Value must be positive!");
+            }
+            this.Amount -= amount;
+        }
+    }
+}
