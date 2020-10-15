e379bc1c6b49bd1e7cba43a09cab5071048f4ede
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-10-15 12:54:26 +0300

    Update Loops Solution

---
+++ Year_2020_2021/11B_Solutions/01-Module OOP/S03-Loops/P04-DemoExamTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P04_DemoExamTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var num = 15; //int
+
+            var num1 = "15"; //string
+
+            var num2 = 15.2d; //double
+
+            var num3 = 45.36m; //decimle
+
+            var o = '0';
+
+            var isNum = 5 > 2; //bool
+
+
+        }
+    }
+}
