e41d998bb176b4c43c81c18e9f4a91078a701fad
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-10-30 11:47:17 +0200

    Working with Console

--- M1-ProgrammingBasics/S01-IntroCSharp/P05-RectangleArea/Program.cs
+++ M1-ProgrammingBasics/S01-IntroCSharp/P05-RectangleArea/Program.cs
@@ -3,15 +3,15 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter a:");
+        //Console.Write("Enter a:");
         double a = double.Parse(Console.ReadLine());
-        Console.Write("Enter b:");
+        //Console.Write("Enter b: ");
         double b = double.Parse(Console.ReadLine());
 
         double s = a * b;
 
-        Console.WriteLine($"S={s}");
-
+        //Console.WriteLine($"S = {s}");
+        Console.WriteLine(s);
     }
 }
 
