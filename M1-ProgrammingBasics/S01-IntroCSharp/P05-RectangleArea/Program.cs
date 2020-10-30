fac1c606de964adde0cbf3fef561809bc661ac1e
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-10-30 10:57:03 +0200

    Add new project to CSharp intro!

---
+++ M1-ProgrammingBasics/S01-IntroCSharp/P05-RectangleArea/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a:");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Enter b:");
+        double b = double.Parse(Console.ReadLine());
+
+        double s = a * b;
+
+        Console.WriteLine($"S={s}");
+
+    }
+}
+
