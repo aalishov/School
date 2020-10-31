056355a86559517c4eb0726e290a6fd4ccfae0ee
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-10-31 11:15:38 +0200

    Add new solution S04-ConditionalStatements.sln

---
+++ M1-ProgrammingBasics/S04-ConditionalStatements/P01-ExcellentResult/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double grade = double.Parse(Console.ReadLine());
+        bool isExcellent = grade >= 5.5;
+        if (isExcellent)
+        {
+            Console.WriteLine("Excellent!");
+        }
+        else
+        {
+            Console.WriteLine("Not excellent.");
+        }
+    }
+}
+
---
+++ M1-ProgrammingBasics/S04-ConditionalStatements/P03-MaxOf2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int firstNum = int.Parse(Console.ReadLine());
+        int secondNum = int.Parse(Console.ReadLine());
+
+        if (firstNum > secondNum)
+        {
+            Console.WriteLine(firstNum);
+        }
+        else
+        {
+            Console.WriteLine(secondNum);
+        }
+
+        //Console.WriteLine(Math.Max(firstNum, secondNum));
+    }
+}
+
