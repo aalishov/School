b944cbf629494217e6bc0776a6e374307a3470a7
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-11-01 13:49:21 +0200

    New projects with for loops!

---
+++ M1-ProgrammingBasics/S07-Loops/P07-SameTwins/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int lastSum = 0;
+        int maxDiff = 0;
+        bool isDifferent = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int currentSum = num1 + num2;
+            int currentDiff = 0;
+
+            if (i==0)
+            {
+                sum = currentSum;
+                lastSum = currentSum;
+            }
+            else if (sum!=currentSum)
+            {
+                isDifferent = true;
+            }
+            currentDiff = Math.Abs(lastSum - currentSum);
+
+            lastSum = currentSum;
+            if (currentDiff>maxDiff)
+            {
+                maxDiff = currentDiff;
+            }
+        }
+
+
+        if (isDifferent)
+        {
+            Console.WriteLine($"No, maxdiff={maxDiff}");
+        }
+        else
+        {
+            Console.WriteLine($"Yes, value={sum}");
+        }
+    }
+}
+
