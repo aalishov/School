fad201ba282644c685ba45dc8dfd403e76b88f96
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-11-02 10:35:05 +0200

    Add new solution S08-DrawingWithLoops.sln

---
+++ M1-ProgrammingBasics/S08-DrawingWithLoops/P03-Triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(new string('$',i));
+        }
+    }
+}
+
