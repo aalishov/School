1a8663cf60389ee7c302c79dfabbed2d0a161896
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-11-04 11:14:57 +0200

    Add 8b solution!

---
+++ Year_2020_2021/8B_Solutions/S01-CSharp-Intro/P01-SumNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    // Ctrl+F5 - Стартира приложението
+    // Ctrl+K+D - Форматиране на кода (подравняване)
+    static void Main()
+    {
+        int num1 = 15;
+        int num2 = 20;
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"{num1} + {num2} = {sum}");
+    }
+}
+
