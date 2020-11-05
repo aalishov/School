0e366610d0014aa81032939513f1800309913e0f
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-11-05 12:59:48 +0200

    Add project11B_Solutions/01-Module OOP/S04-IntroOOP/P05-School/P05-School.csproj

---
+++ Year_2020_2021/11B_Solutions/01-Module OOP/S04-IntroOOP/P05-School/School.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class School
+{
+    public School(string name)
+    {
+        this.Name = name;
+        this.Students = new List<Student>();
+    }
+    public string Name { get; set; }
+    public int StudentsCount { get => this.Students.Count; }
+
+    public List<Student> Students { get; set; }
+
+    public string Info()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"=> School: {this.Name}");
+        sb.AppendLine($"- Students count: {this.StudentsCount}");
+
+        foreach (var student in this.Students)
+        {
+            sb.AppendLine(student.Info());
+            sb.AppendLine(new string('*', 30));
+        }
+
+        return sb.ToString().TrimEnd();
+    }
+}
+
