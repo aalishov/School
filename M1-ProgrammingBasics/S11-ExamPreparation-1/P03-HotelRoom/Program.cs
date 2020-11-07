7bf7797a345d56123328c56900c7b00ecddec7a9
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-11-07 11:45:52 +0200

    Starting exam preparation!

---
+++ M1-ProgrammingBasics/S11-ExamPreparation-1/P03-HotelRoom/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string month = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+
+        decimal studioPrice = 0.0m;
+        decimal apartmentPrice = 0.0m;
+
+        switch (month)
+        {
+            case "May":
+            case "October":
+                apartmentPrice = 65m;
+                studioPrice = 50m;
+                if (n > 14)
+                {
+                    studioPrice *= 0.7m;
+                }
+                else if (n > 7)
+                {
+                    studioPrice *= 0.95m;
+                }
+                break;
+            case "June":
+            case "September":
+                apartmentPrice = 68.7m;
+                studioPrice = 75.2m;
+                if (n>14)
+                {
+                    studioPrice *= 0.8m;
+                }
+                break;
+            case "July":
+            case "August":
+                apartmentPrice = 77m;
+                studioPrice = 76m;
+                break;
+        }
+
+        if (n>14)
+        {
+            apartmentPrice *= 0.9m;
+        }
+
+        decimal apartmentTotal = n * apartmentPrice;
+        decimal studioTotal = n * studioPrice;
+
+        Console.WriteLine($"Apartment: {apartmentTotal:f2} lv.");
+        Console.WriteLine($"Studio: {studioTotal:f2} lv.");
+
+    }
+}
+
