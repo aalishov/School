41bf3ba4753e0d72738497b533b7f4923011d4a4
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-11-14 12:31:09 +0200

    Exam preparation 3!

---
+++ M1-ProgrammingBasics/S13-ExamPreparation-3/P03-SeasonalSale/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string model = Console.ReadLine();
+        string carType = Console.ReadLine();
+        string season = Console.ReadLine();
+        string condition = Console.ReadLine();
+        decimal price = decimal.Parse(Console.ReadLine());
+        decimal wantedProfit = decimal.Parse(Console.ReadLine());
+
+        decimal profitPercent = decimal.Zero;
+
+        switch (condition)
+        {
+            case "perfect":
+                //if (carType == "suv")
+                //{
+                //    profitPercent = 0.3m;
+                //}
+                //else
+                //{
+                //    profitPercent = 0.25m;
+                //}
+                profitPercent = SetPercentValue(carType, 0.3m, 0.25m);
+                break;
+            case "good":
+                //if (carType == "suv")
+                //{
+                //    profitPercent = 0.2m;
+                //}
+                //else
+                //{
+                //    profitPercent = 0.15m;
+                //}
+                profitPercent = SetPercentValue(carType, 0.2m, 0.15m);
+                break;
+            case "bad":
+                profitPercent = 0.1m;
+                break;
+        }
+
+        decimal profit = price * profitPercent;
+
+        if (season=="winter")
+        {
+            profit -= 200;
+        }
+
+        if (profit>=wantedProfit)
+        {
+            Console.WriteLine($"The profit on {model} will be good - {profit:f2} BGN");
+        }
+        else
+        {
+            decimal x = wantedProfit - profit;
+            Console.WriteLine($"The car is not worth selling now{Environment.NewLine}Need {x:f2} more profit");
+        }
+    }
+
+    private static decimal SetPercentValue(string carType, decimal percentSuv, decimal percentSedan)
+    {
+        decimal profitPercent;
+        if (carType == "suv")
+        {
+            profitPercent = percentSuv;
+        }
+        else
+        {
+            profitPercent = percentSedan;
+        }
+
+        return profitPercent;
+    }
+}
+
---
+++ M1-ProgrammingBasics/S13-ExamPreparation-3/P05-CarNumbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        string start = Console.ReadLine();
+        string end = Console.ReadLine();
+        long n = long.Parse(Console.ReadLine());
+
+        long numberCounter = 0;
+        bool isGenerated = false;
+
+        for (int i = 0; i <= 9; i++)
+        {
+            if (isGenerated)
+            {
+                break;
+            }
+            for (int j = 0; j <= 9; j++)
+            {
+                if (isGenerated)
+                {
+                    break;
+                }
+                for (int k = 0; k <= 9; k++)
+                {
+                    if (isGenerated)
+                    {
+                        break;
+                    }
+                    for (int l = 0; l <= 9; l++)
+                    {
+                        long sum = i + j + k + l;
+                        long product = i * k-n;
+                        if (sum == product)
+                        {
+                            numberCounter++;
+                            Console.Write($"{start}{i}{j}{k}{l}{end} ");
+                        }
+                        if (numberCounter == n)
+                        {
+                            isGenerated = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
