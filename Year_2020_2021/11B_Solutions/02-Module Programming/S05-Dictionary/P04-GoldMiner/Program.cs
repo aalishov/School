4a3b5ee72b4f4d78cd2ac88255059c5a7f47fd27
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-12-02 12:05:48 +0200

    8Б string methods!

---
+++ Year_2020_2021/11B_Solutions/02-Module Programming/S05-Dictionary/P04-GoldMiner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> resources = new Dictionary<string, int>();
+        resources.Add("gold", 0);
+        resources.Add("silver", 0);
+        resources.Add("copper", 1);
+
+        while (true)
+        {
+            string resourceName = Console.ReadLine().ToLower();
+            if (resourceName=="stop")
+            {
+                break;
+            }
+            int value = int.Parse(Console.ReadLine());
+            resources[resourceName] += value;
+        }
+
+        foreach (var resource in resources)
+        {
+            Console.WriteLine($"{resource.Key} -> {resource.Value}");
+        }
+    }
+}
+
