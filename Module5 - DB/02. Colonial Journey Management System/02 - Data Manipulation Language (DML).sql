1f867adbf7adfc28773a3e98f3f8ee8d684c02a3
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-12-06 11:58:47 +0200

    Finish first exam preparation!

--- Module5 - DB/02. Colonial Journey Management System/02 - Data Manipulation Language (DML).sql
+++ Module5 - DB/02. Colonial Journey Management System/02 - Data Manipulation Language (DML).sql
@@ -9,9 +9,38 @@ insert into travel_cards (id, card_number, job_during_journey, colonist_id, jour
 insert into travel_cards (id, card_number, job_during_journey, colonist_id, journey_id) values (100, '1967015685', 'Pilot', 100, 1);
 
 -- 4.    Обновяване на данни
+-- Ако id-то на пътуването е делим на 2 без остатък – ‘Medical’.
 select * from journeys
 where id mod 2 =  0;
 
 update journeys 
 set purpose = 'Medical'
 where id mod 2 =  0;
+-- Ако id-то на пътуването е делим на 3 без остатък – ‘Technical’.
+select * from journeys
+where id mod 3 =  0;
+
+update journeys 
+set purpose = 'Technical'
+where id mod 3 =  0;
+-- Ако id-то на пътуването е делим на 5 без остатък – ‘Educational’.
+select * from journeys
+where id mod 5 =  0;
+
+update journeys 
+set purpose = 'Educational'
+where id mod 5 =  0;
+-- Ако id-то на пътуването е делим на 7 без остатък – ‘Military’.
+select * from journeys
+where id mod 7 =  0;
+
+update journeys 
+set purpose = 'Military'
+where id mod 7 =  0;
+
+-- 5.    Изтриване на данни
+delete from colonists
+where id not in(select colonist_id from travel_cards);
+
+
+
