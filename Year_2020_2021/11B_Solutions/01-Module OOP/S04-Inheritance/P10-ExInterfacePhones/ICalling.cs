9696b248a96e6c0a95aee95c53ff4e1504e33274
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2021-01-03 21:40:49 +0200

    Inheritance exercise!

---
+++ Year_2020_2021/11B_Solutions/01-Module OOP/S04-Inheritance/P10-ExInterfacePhones/ICalling.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P10_ExInterfacePhones
+{
+    public interface ICalling
+    {
+        public string Call(string phoneNumber);
+    }
+}
