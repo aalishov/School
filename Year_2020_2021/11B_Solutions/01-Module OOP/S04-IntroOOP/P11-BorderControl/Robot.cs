add9162e82032b0d529166062b648d75bfadaeee
Alish Alishov <a.alishov@live.com>
2021-01-11 10:20:38 +0200

    Polumorphism ex 11b!

---
+++ Year_2020_2021/11B_Solutions/01-Module OOP/S04-IntroOOP/P11-BorderControl/Robot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P11_BorderControl
+{
+    public class Robot
+    {
+        public string Id { get; set; }
+
+        public string Model { get; set; }
+    }
+}
---
+++ Year_2020_2021/11B_Solutions/01-Module OOP/S05-Polymorphism/P02-Animals/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P02_Animals
+{
+    using System;
+    public class Program
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog("Spyke", "bones");
+            Console.WriteLine(dog.ExplainMySelf());
+
+            Cat cat = new Cat("Tom", "chicken");
+            Console.WriteLine(cat.ExplainMySelf());
+        }
+    }
+}
