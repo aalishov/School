7d4b091dc09fa407506b0be1e99cbac8ca656500
Alish Alishov <a.alishov@live.com>
2021-01-14 12:36:27 +0200

    Machines project!

---
+++ Year_2020_2021/11B_Solutions/01-Module OOP/S05-Polymorphism/MachinesCommon/MachineOperator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MachinesCommon
+{
+    public class MachineOperator
+    {
+        public MachineOperator(IMachine entity)
+        {
+            this.Entity = entity;
+        }
+
+        private IMachine entity;
+        public IMachine Entity
+        {
+            get { return this.entity; }
+            set
+            {
+                this.entity = value;
+            }
+        }
+
+        public string ChangeMessage()
+        {
+            return "Machine operator is operating: " + entity.MachineType;
+        }
+
+        public string Start()
+        {
+            return Entity.Start();
+        }
+
+        public string Stop()
+        {
+            return Entity.Stop();
+        }
+
+
+    }
+}
