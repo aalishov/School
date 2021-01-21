440ebd633b33f2ffb095ba109dfadf14e395c6a3
Alish Alishov <a.alishov@live.com>
2021-01-21 09:36:31 +0200

    Class 10B - String Processing

---
+++ Year_2020_2021/10B_Solutions/S03-StringProcessing/P08-DemoBuilder/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string words = "Hello, string processing string with string C#! string";
+
+        StringBuilder builder = new StringBuilder(words);
+
+        builder[1] = 'A';
+        builder.Replace("string", "******");
+        builder.AppendLine(" new text");
+        builder.Append(" new text 2");
+
+        for (int i = 0; i < 50; i++)
+        {
+            if (i % 2 == 0)
+            {
+                builder.AppendLine($"\t {i} => text");
+            }
+            else
+            {
+                builder.Append($"{i} => text");
+            }
+           
+        }
+        //builder.Remove(20,5);
+        Console.WriteLine(builder);
+        
+    }
+}
+
---
+++ Year_2020_2021/9B_Solutions/S05-ConditionalStatementsAdvanced/P02-DemoSwitchCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        switch (n)
+        {
+            case 1:
+                Console.WriteLine("one");
+                break;
+            case 2:
+                Console.WriteLine("two");
+                break;
+            case 3:
+                Console.WriteLine("three");
+                break;
+            case 4:
+                Console.WriteLine("four");
+                break;
+            case 5:
+                Console.WriteLine("five");
+                break;
+            case 6:
+                Console.WriteLine("six");
+                break;
+            case 7:
+                Console.WriteLine("seven");
+                break;
+            default:
+                Console.WriteLine("Error...");
+                break;
+        }
+    }
+
+}
+
