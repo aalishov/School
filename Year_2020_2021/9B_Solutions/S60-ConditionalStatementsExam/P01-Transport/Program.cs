af9e97e44b57beec7526e5a96e34d0818b1534fc
Alish Alishov <a.alishov@live.com>
2021-02-04 12:08:50 +0200

    Class 9B - Conditional statements ex - transport!

---
+++ Year_2020_2021/9B_Solutions/S60-ConditionalStatementsExam/P01-Transport/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double km = double.Parse(Console.ReadLine());
+        string dayOrNight = Console.ReadLine();
+
+        double totalPrice = 0;
+
+        if (km < 20)
+        {
+            //taxi
+            totalPrice += 0.70;
+
+            if (dayOrNight == "day")
+            {
+                totalPrice += km * 0.79;
+            }
+            else if (dayOrNight == "night")
+            {
+                totalPrice += km * 0.9;
+            }
+        }
+        else if (km >= 20 && km < 100)
+        {
+            //bus
+            totalPrice += km * 0.09;
+        }
+        else if (km >= 100)
+        {
+            //train
+            totalPrice += km * 0.06;
+        }
+
+        Console.WriteLine($"{totalPrice:f2}");
+    }
+}
+
