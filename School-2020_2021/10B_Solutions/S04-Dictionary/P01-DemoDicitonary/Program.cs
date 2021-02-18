ac7bdcdf8a06f52a5ece4a29c5284fcc3e9f0b87
Alish Alishov <a.alishov@live.com>
2021-02-18 20:11:01 +0200

    10B class - Dictionary

---
+++ School-2020_2021/10B_Solutions/S04-Dictionary/P01-DemoDicitonary/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+        phoneBook.Add("Ivan", "+3598888");
+        phoneBook.Add("Gosho", "454545");
+
+        while (true)
+        {
+            Console.Write("Enter command: ");
+            string cmd = Console.ReadLine();
+
+            switch (cmd)
+            {
+                case "Add":
+                    AddEntry(phoneBook);
+                    break;
+                case "Remove":
+                     RemoveEntry(phoneBook);
+                    break;
+                case "Call":
+                    Calling(phoneBook);
+                    break;
+                case "Print":
+                    foreach (var name in phoneBook)
+                    {
+                        Console.WriteLine($"{ name.Key} => {name.Value}");
+                    }
+                    break;
+            }
+        }
+    }
+
+    private static void Calling(Dictionary<string, string> phoneBook)
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        if (phoneBook.ContainsKey(name))
+        {
+            Console.WriteLine($"Calling {phoneBook[name]}");
+        }
+        else
+        {
+            Console.WriteLine("Not found");
+        }
+    }
+
+    private static void RemoveEntry(Dictionary<string, string> phoneBook)
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        if (phoneBook.ContainsKey(name))
+        {
+            phoneBook.Remove(name);
+            Console.WriteLine("Removed");
+        }
+        else
+        {
+            Console.WriteLine("Not found");
+        }
+    }
+
+    private static void AddEntry(Dictionary<string, string> phoneBook)
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter phone: ");
+        string phone = Console.ReadLine();
+        if (phoneBook.ContainsKey(name))
+        {
+            phoneBook[name] = phone;
+        }
+        else
+        {
+            phoneBook.Add(name, phone);
+        }
+    }
+}
+
---
+++ School-2020_2021/10B_Solutions/S04-Dictionary/P02-NestedDicitonary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02_NestedDicitonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, List<int>>> grades = new Dictionary<string, Dictionary<string, List<int>>>();
+            Dictionary<string, List<int>> peshoGrades = new Dictionary<string, List<int>>();
+            peshoGrades.Add("Math", new List<int>() { 3, 5, 6, 6 });
+            peshoGrades.Add("History", new List<int>() { 3, 5, 6, 6 });
+
+            Dictionary<string, List<int>> goshoGrades = new Dictionary<string, List<int>>();
+            goshoGrades.Add("Math", new List<int>() { 3, 5, 6, 6 });
+            goshoGrades.Add("History", new List<int>() { 3, 5, 6, 6 });
+            goshoGrades.Add("Music", new List<int>() { 2, 5, 2, 6 });
+
+            grades.Add("Pesho", peshoGrades);
+            grades.Add("Gosho", goshoGrades);
+
+            foreach (var name in grades)
+            {
+                Console.WriteLine(name.Key);
+                foreach (var subject in name.Value)
+                {
+                    Console.WriteLine($"\t{subject.Key} => {string.Join(", ", subject.Value)}");
+                }
+            }
+        }
+    }
+}
