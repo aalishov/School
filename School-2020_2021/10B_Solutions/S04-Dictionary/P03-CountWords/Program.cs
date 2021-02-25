db5a7887ce8c2a5de632293850cc7b4637d52555
Alish Alishov <a.alishov@live.com>
2021-02-25 11:53:07 +0200

    Class 10B - Dictionary exercise

---
+++ School-2020_2021/10B_Solutions/S04-Dictionary/P03-CountWords/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> input = Console
+            .ReadLine()
+            .ToLower()
+            .Split(' ')
+            .ToList();
+        Dictionary<string, int> words = new Dictionary<string, int>();
+
+        foreach (var word in input)
+        {
+            if (words.ContainsKey(word))
+            {
+                words[word]++;
+            }
+            else
+            {
+                words.Add(word, 1);
+            }
+        }
+        foreach (var word in words)
+        {
+            if (word.Value%2==1)
+            {
+                Console.WriteLine($"{word.Key} => {word.Value}");
+            }
+        }
+    }
+}
+
