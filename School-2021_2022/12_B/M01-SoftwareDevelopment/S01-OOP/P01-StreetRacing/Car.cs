464cc91940eb5a0d96cbe1a3dc56ca69ef82f3f5
Alish Alishov <a.alishov@live.com>
2021-09-24 11:41:04 +0300

    Exercise 12b software development 24/09/2021 - CarRacins

---
+++ School-2021_2022/12_B/M01-SoftwareDevelopment/S01-OOP/P01-StreetRacing/Car.cs
@@ -0,0 +1,32 @@
+﻿namespace P01_StreetRacing
+{
+    using System.Text;
+    public class Car
+    {
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+
+        public string Make { get; private set; }
+        public string Model { get; private set; }
+        public string LicensePlate { get; private set; }
+        public int HorsePower { get; private set; }
+        public double Weight { get; private set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {this.Make}");
+            sb.AppendLine($"Model: {this.Model}");
+            sb.AppendLine($"License Plate: {this.LicensePlate}");
+            sb.AppendLine($"Horse Power: {this.HorsePower}");
+            sb.AppendLine($"Weight: {this.Weight}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ School-2021_2022/12_B/M01-SoftwareDevelopment/S01-OOP/P01-StreetRacing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P01_StreetRacing
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car car = new Car("BMW", "320ci", "NFS2005", 150, 1450);
+            Console.WriteLine(car);
+        }
+    }
+}
