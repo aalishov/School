250b0c88b2dc0d558f9653437a1861321f40fe77
Alish Alishov <a.alishov@live.com>
2021-09-29 11:34:32 +0300

    Exercise 12B SoftwareDevelopment StreetRacing Part2

--- School-2021_2022/12_B/M01-SoftwareDevelopment/S01-OOP/P01-StreetRacing/Program.cs
+++ School-2021_2022/12_B/M01-SoftwareDevelopment/S01-OOP/P01-StreetRacing/Program.cs
@@ -7,7 +7,16 @@ namespace P01_StreetRacing
         static void Main()
         {
             Car car = new Car("BMW", "320ci", "NFS2005", 150, 1450);
-            Console.WriteLine(car);
+            Car car1 = new Car("Audi", "A5", "NFS2009", 130, 1600);
+            Car car2 = new Car("Audi", "A6", "NFS2007", 140, 1500);
+            Car car3 = new Car("Opel", "Insignia", "NFS2008", 190, 1650);
+
+            Race race = new Race("RockPort Race", "Sprint", 1, 4, 150);
+            race.Add(car);
+            race.Add(car1);
+            race.Add(car2);
+            Console.WriteLine(race.Remove("NFS2005"));
+            Console.WriteLine(race.Report());
         }
     }
 }
---
+++ School-2021_2022/12_B/M01-SoftwareDevelopment/S01-OOP/P01-StreetRacing/Race.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P01_StreetRacing
+{
+    public class Race
+    {
+        private readonly List<Car> participants;
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+            this.participants = new List<Car>();
+        }
+
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public int Laps { get; private set; }
+        public int Capacity { get; private set; }
+        public int MaxHorsePower { get; private set; }
+        public int Count { get { return this.participants.Count; } }
+        public void Add(Car car)
+        {
+            if (this.participants.Any(x => x.LicensePlate == car.LicensePlate)) { return; }
+            if (this.Count >= this.Capacity) { return;}
+            if (car.HorsePower > this.MaxHorsePower)  { return; }
+            this.participants.Add(car);
+        }
+        public bool Remove(string licensePlate)
+        {
+            Car car = FindParticipant(licensePlate);
+            return this.participants.Remove(car);
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            return this.participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        }
+        public Car GetMostPowerfulCar()
+        {
+            return this.participants
+                .OrderByDescending(x => x.HorsePower)
+                .FirstOrDefault();
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {this.Name} - Type: {this.Type} (Laps: {this.Laps})");
+            foreach (var car in this.participants)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
