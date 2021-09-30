b01bf4201cbcff240d0de47a5780520ed4062524
Alishov <a.alishov@live.com>
2021-09-30 07:58:48 +0300

    Create folder 9_B with solution IntroToProgramming

---
+++ School-2021_2022/9_B/IntroToProgramming/P01-FirstConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_SecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First console app!");
+        }
+    }
+}
---
+++ School-2021_2022/9_B/IntroToProgramming/P02-SecondConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P02_SecondConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second console app!");
+        }
+    }
+}
