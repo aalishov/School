04d26745954158f3329d3e6dc06b9733a4e9d943
Alishov <a.alishov@live.com>
2021-09-30 08:45:00 +0300

    Ex 9b 30/09/2021

---
+++ School-2021_2022/9_B/IntroToProgramming/P03-IntroConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P03_IntroConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ School-2021_2022/9_B/IntroToProgramming/P04-SquareArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P04_SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine());
+            int area = a * a;
+            Console.WriteLine($"Square = {area}");
+        }
+    }
+}
