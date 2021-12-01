4cc33f9e8284f88d310e44b328f59f9ac22ac5c3
Alishov <a.alishov@live.com>
2021-12-01 14:20:05 +0200

    Exam prep Module 10

--- IT-Career-V3/Module 10 - ASD/ExamPreparation/Sum/Sum/Program.cs
+++ IT-Career-V3/Module 10 - ASD/ExamPreparation/Sum/Sum/Program.cs
@@ -5,12 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        int s = 0;
-        for (int i = 1; i <= 1000; i++)
-        {
-            s += i;
-        }
-        Console.WriteLine(s);
+        Console.WriteLine(Math.Sqrt(8)+Math.PI);
+        Console.WriteLine(Math.Pow(2,10));
+        Console.WriteLine(Math.PI);
+
+        //int s = 0;
+        //for (int i = 1; i <= 1000; i++)
+        //{
+        //    s += i;
+        //}
+        //Console.WriteLine(s);
     }
 }
 
