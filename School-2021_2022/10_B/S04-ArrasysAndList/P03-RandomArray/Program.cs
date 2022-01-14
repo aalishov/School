6ab2effb80818edb71ed6cef4372592410444341
Alishov <a.alishov@live.com>
2022-01-14 11:45:06 +0200

    Programming 10 class - exercise arrays and list

--- School-2021_2022/10_B/S04-ArrasysAndList/P03-RandomArray/Program.cs
+++ School-2021_2022/10_B/S04-ArrasysAndList/P03-RandomArray/Program.cs
@@ -10,6 +10,31 @@ public class Program
         Console.WriteLine(string.Join(", ", nums));
         Console.WriteLine(new string('*', 30));
         Console.WriteLine($"Sum abs 30: {SumAbs30(nums)}");
+        Console.WriteLine($"Product odd: {ProductOdd(nums)}");
+        Console.WriteLine($"Product even: {ProductEven(nums)}");
+        Console.WriteLine($"Count even: {CountEvenIndex(nums)}");
+    }
+    public static int CountEvenIndex(List<int> nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Count; i+=2)
+        {
+            if (nums[i] >= -30 && nums[i] <= 30)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int ProductEven(List<int> nums)
+    {
+        List<int> filter = nums.Where(x => x % 2 == 0 && x != 0).ToList();
+        int product = 1;
+        for (int i = 0; i < filter.Count; i++)
+        {
+            product *= filter[i];
+        }
+        return product;
     }
     public static int ProductOdd(List<int> nums)
     {
@@ -29,7 +54,7 @@ public class Program
     {
         Random r = new Random();
         List<int> nums = new List<int>();
-        for (int i = 0; i < 50; i++)
+        for (int i = 0; i <8; i++)
         {
             nums.Add(r.Next(-100, 100));
         }
