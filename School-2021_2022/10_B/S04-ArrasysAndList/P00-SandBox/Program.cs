93acfa895a1262eb94879e943f18a21c2bd81844
Alishov <a.alishov@live.com>
2022-01-19 09:37:56 +0200

    Programming 10B exam preparation

---
+++ School-2021_2022/10_B/S04-ArrasysAndList/P00-SandBox/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P00_SandBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>(new[] { 3, 5, 7, 12, 8, 7 });
+            List<int> filter = new[] { 3, 7, 12, 8, }.Where(x => x % 2 == 0).ToList();
+            int count = new[] { -30, 7, 50, 8}.Where(x => Math.Abs(x) > 30).Count();
+            Console.WriteLine(count);
+        }
+    }
+}
