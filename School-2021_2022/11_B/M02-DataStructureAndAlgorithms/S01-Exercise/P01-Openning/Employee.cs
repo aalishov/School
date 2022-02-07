8c9367d05c53f87a1ff406549fbc6d9ce0bcb0ed
Alishov <a.alishov@live.com>
2022-02-07 12:50:23 +0200

    Exercise 07/02/2022

---
+++ School-2021_2022/11_B/M02-DataStructureAndAlgorithms/S01-Exercise/P01-Openning/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace P01_Openning
+{
+    public class Employee
+    {
+        public Employee(string name, int age, string country)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Country = country;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: {this.Name}, {this.Age} ({this.Country})";
+        }
+    }
+}
