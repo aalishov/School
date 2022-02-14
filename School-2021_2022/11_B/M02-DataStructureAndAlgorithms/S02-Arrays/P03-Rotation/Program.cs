45b0d5af6c4929daade47c63a825e3891b3c8106
Alishov <a.alishov@live.com>
2022-02-14 13:27:12 +0200

    Exercise 11 class

---
+++ School-2021_2022/11_B/M02-DataStructureAndAlgorithms/S02-Arrays/P03-Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] nums = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int first = nums[0];
+            for (int j = 0; j < nums.Length-1; j++)
+            {
+                nums[j] = nums[j + 1];
+            }
+            nums[nums.Length - 1] = first;
+        }
+        Console.WriteLine(string.Join(" ",nums));
+    }
+}
+
