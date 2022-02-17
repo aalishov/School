712a1c3b1a6b86c5795ca9d1e7c1afcd6a57d76a
aalishov <a.alishov@live.com>
2022-02-17 09:37:42 +0200

    Exercise 9b class

---
+++ School-2021_2022/9_B/S06-ConditionalStatementsExams/P03-Tom/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int relaxingDays = int.Parse(Console.ReadLine());
+        int workingDays = 365 - relaxingDays;
+
+        int workingDaysPlay = workingDays * 63;
+        int relaxingDaysPlay = relaxingDays * 127;
+        int totalPlay = workingDaysPlay + relaxingDaysPlay;
+
+        int diff = Math.Abs(totalPlay - 30000);
+        int hours = diff / 60;
+        int minutes = diff % 60;
+
+        if (totalPlay > 30000)
+        {
+            Console.WriteLine("Tom will run away");
+            Console.WriteLine($"{hours} hours and {minutes} more for play");
+        }
+        else
+        {
+            Console.WriteLine($"Tom sleeps well");
+            Console.WriteLine($"{hours} hours and {minutes} minutes less for play");
+        }
+    }
+}
---
+++ School-2021_2022/9_B/S06-ConditionalStatementsExams/P04-Harvest/Program.cs
@@ -0,0 +1,20 @@
+﻿//Input
+int x = int.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+int z = int.Parse(Console.ReadLine());
+int workers = int.Parse(Console.ReadLine());
+
+double totalGrapes = x * y;
+double grapesWine = totalGrapes * 0.4;
+double wine = grapesWine / 2.5;
+
+double litersLeft = Math.Abs(Math.Ceiling(wine - z));
+if (wine > z)
+{
+    double winePerWorker = litersLeft / workers;
+    Console.WriteLine($"Good harvest this year! Total wine: {wine} liters. {litersLeft} liters left -> {winePerWorker} liters per person.");
+}
+else
+{
+    Console.WriteLine($"It will be a tough winter! More {litersLeft} liters wine needed.");
+}
