4c9e14c8ec02e4300ae939c9095e4689f080604d
Alishov <a.alishov@live.com>
2022-02-23 14:12:02 +0200

    Exercise 8 class - Students

---
+++ School-2021_2022/8_B/S02-ConditionalStatementsAndLoops/P03-Students/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P03_Students
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            int girlsA = int.Parse(textBoxGirlsA.Text);
+            int boysA = int.Parse(textBoxBoysA.Text);
+            int girlsB = int.Parse(textBoxGirlsB.Text);
+            int boysB = int.Parse(textBoxBoysB.Text);
+            int girlsV = int.Parse(textBoxGirlsV.Text);
+            int boysV = int.Parse(textBoxBoysV.Text);
+            int total = 0;
+            if (radioButtonGirls.Checked)
+            {
+                if (checkBoxClassA.Checked)
+                {
+                    total += girlsA;
+                }
+                if (checkBoxClassB.Checked)
+                {
+                    total += girlsB;
+                }
+                if (checkBoxClassV.Checked)
+                {
+                    total += girlsV;
+                }
+            }
+            else if (radioButtonBoys.Checked)
+            {
+                if (checkBoxClassA.Checked)
+                {
+                    total += boysA;
+                }
+                if (checkBoxClassB.Checked)
+                {
+                    total += boysB;
+                }
+                if (checkBoxClassV.Checked)
+                {
+                    total += boysV;
+                }
+            }
+            else
+            {
+                if (checkBoxClassA.Checked)
+                {
+                    total += boysA+girlsA;
+                }
+                if (checkBoxClassB.Checked)
+                {
+                    total += boysB+girlsB;
+                }
+                if (checkBoxClassV.Checked)
+                {
+                    total += boysV+girlsV;
+                }
+            }
+            labelResult.Text = total.ToString();
+        }
+    }
+}
