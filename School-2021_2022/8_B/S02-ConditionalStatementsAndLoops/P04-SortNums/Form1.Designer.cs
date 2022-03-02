205526dbec09bbe80d12a4fef414cb6754dff2e2
Alishov <a.alishov@live.com>
2022-03-02 14:17:28 +0200

    Exercise 8b

---
+++ School-2021_2022/8_B/S02-ConditionalStatementsAndLoops/P04-SortNums/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace P04_SortNums
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.radioButtonAsc = new System.Windows.Forms.RadioButton();
+            this.radioButtonDesc = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 38);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 38);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C=";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(100, 152);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(163, 43);
+            this.textBoxA.TabIndex = 3;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(100, 201);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(163, 43);
+            this.textBoxB.TabIndex = 4;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(100, 253);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(163, 43);
+            this.textBoxC.TabIndex = 5;
+            // 
+            // radioButtonAsc
+            // 
+            this.radioButtonAsc.AutoSize = true;
+            this.radioButtonAsc.Location = new System.Drawing.Point(19, 42);
+            this.radioButtonAsc.Name = "radioButtonAsc";
+            this.radioButtonAsc.Size = new System.Drawing.Size(81, 42);
+            this.radioButtonAsc.TabIndex = 6;
+            this.radioButtonAsc.TabStop = true;
+            this.radioButtonAsc.Text = "Asc";
+            this.radioButtonAsc.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDesc
+            // 
+            this.radioButtonDesc.AutoSize = true;
+            this.radioButtonDesc.Location = new System.Drawing.Point(106, 42);
+            this.radioButtonDesc.Name = "radioButtonDesc";
+            this.radioButtonDesc.Size = new System.Drawing.Size(98, 42);
+            this.radioButtonDesc.TabIndex = 7;
+            this.radioButtonDesc.TabStop = true;
+            this.radioButtonDesc.Text = "Desc";
+            this.radioButtonDesc.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonAsc);
+            this.groupBox1.Controls.Add(this.radioButtonDesc);
+            this.groupBox1.Location = new System.Drawing.Point(30, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(233, 104);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sort";
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(26, 316);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(144, 70);
+            this.buttonSort.TabIndex = 9;
+            this.buttonSort.Text = "Sort";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(186, 320);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(107, 65);
+            this.buttonClose.TabIndex = 10;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(142, 399);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(28, 38);
+            this.labelResult.TabIndex = 11;
+            this.labelResult.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 446);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.RadioButton radioButtonAsc;
+        private System.Windows.Forms.RadioButton radioButtonDesc;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ School-2021_2022/8_B/S02-ConditionalStatementsAndLoops/P04-SortNums/Form1.cs
@@ -0,0 +1,82 @@
+﻿namespace P04_SortNums
+{
+    using System;
+    using System.Windows.Forms;
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBoxA.Text);
+            double b = double.Parse(textBoxB.Text);
+            double c = double.Parse(textBoxC.Text);
+            if (radioButtonAsc.Checked)
+            {
+                if (b > a && c > b)
+                {
+                    labelResult.Text = $"{a} {b} {c}";
+                }
+                else if (c>a && b>c )
+                {
+                    labelResult.Text = $"{a} {c} {b}";
+                }
+                else if (a > b && c>a)
+                {
+                    labelResult.Text = $"{b} {a} {c}";
+                }
+                else if (c > b && a > c)
+                {
+                    labelResult.Text = $"{b} {c} {a}";
+                }
+                else if (a > c && b > a)
+                {
+                    labelResult.Text = $"{c} {a} {b}";
+                }
+                else
+                {
+                    labelResult.Text = $"{c} {b} {a}";
+                }
+            }
+            else if (radioButtonDesc.Checked)
+            {
+                if (b < a && c < b)
+                {
+                    labelResult.Text = $"{a} {b} {c}";
+                }
+                else if (c < a && b < c)
+                {
+                    labelResult.Text = $"{a} {c} {b}";
+                }
+                else if (a < b && c < a)
+                {
+                    labelResult.Text = $"{b} {a} {c}";
+                }
+                else if (c < b && a < c)
+                {
+                    labelResult.Text = $"{b} {c} {a}";
+                }
+                else if (a < c && b < a)
+                {
+                    labelResult.Text = $"{c} {a} {b}";
+                }
+                else
+                {
+                    labelResult.Text = $"{c} {b} {a}";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select ASC or DESC!");
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
