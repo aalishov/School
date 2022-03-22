6a8d0d4de54c74824491fb91044058d254c8e66a
Alishov <a.alishov@live.com>
2022-03-22 08:53:39 +0200

    Exercises

---
+++ School-2021_2022/10_B/S07-Dictionary/P00-SandBox/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        Dictionary<string,int[]> d = new Dictionary<string, int[]>();
+        d.Add("ivan", new int[] { 3,4,6});
+        d.Add("gosho", new int[] { 5, 4, 6 });
+        //Console.WriteLine(string.Join(" ",d));
+        foreach (var item in d)
+        {
+            Console.WriteLine($"Key: {item.Key}, Value: {string.Join(", ", item.Value)}");
+        }
+    }
+}
+
---
+++ School-2021_2022/12_B/M01-SoftwareDevelopment/S05-WebApps/P01-Students_v3/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using P01_Students_v3.Data;
+
+[assembly: HostingStartup(typeof(P01_Students_v3.Areas.Identity.IdentityHostingStartup))]
+namespace P01_Students_v3.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
