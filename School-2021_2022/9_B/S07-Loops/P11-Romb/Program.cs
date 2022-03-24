264d2b710ff3f2a9ffc31d60e4bebb894390ab67
aalishov <a.alishov@live.com>
2022-03-24 07:47:15 +0200

    Exercise 9b class

---
+++ School-2021_2022/9_B/S07-Loops/P11-Romb/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter n: ");
+int n=int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(new String(' ',n-i));
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = n - 1; i > 0; i--)
+{
+    Console.Write(new String(' ',n-i));
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("* ");
+    }
+    Console.WriteLine();
+}
