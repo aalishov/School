fd2cd3c3313e5555066c4b9e9d1f696e99eeac79
Alishov <a.alishov@live.com>
2022-03-26 09:35:40 +0200

    Exercise 8 class

---
+++ School-2021_2022/8_B/S02-ConditionalStatementsAndLoops/P17-DemoFiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main()
+    {
+        using (StreamReader reader = new StreamReader("input.txt"))
+        {
+            string line = "";
+            while (line != null)
+            {
+                line = reader.ReadLine();
+                Console.WriteLine($"=>{line}");
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter("input.txt", true))
+        {
+            Console.Write("Enter brand: ");
+            string brand = Console.ReadLine();
+            writer.WriteLine(brand);
+        }
+    }
+}
+
