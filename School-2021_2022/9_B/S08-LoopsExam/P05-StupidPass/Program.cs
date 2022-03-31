db1c6c31f00ec946302891585794d47f67e4c146
aalishov <a.alishov@live.com>
2022-03-31 09:33:41 +0300

    Exercise 9 class

---
+++ School-2021_2022/9_B/S08-LoopsExam/P05-StupidPass/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int l = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+{
+    for (int j = 1; j < n; j++)
+    {
+        for (int k = 'a'; k < ('a' + l); k++)
+        {
+            for (int m = 'a'; m < ('a' + l); m++)
+            {
+                for (int o = 1; o <= n; o++)
+                {
+                    if (o > i && o > j)
+                    {
+                        Console.WriteLine($"{i}{j}{(char)k}{(char)m}{o}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ School-2021_2022/9_B/S08-LoopsExam/P17-Histograma/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int p1Counter = 0;
+int p2Counter = 0;
+int p3Counter = 0;
+int p4Counter = 0;
+int p5Counter = 0;
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    if (num < 200) { p1Counter++; }
+    else if (num < 400) { p2Counter++; }
+    else if (num < 600) { p3Counter++; }
+    else if (num < 800) { p4Counter++; }
+    else { p5Counter++; }
+}
+Console.WriteLine($"{p1Counter / (double)n * 100.0:f2}%");
+Console.WriteLine($"{p2Counter / (double)n * 100.0:f2}%");
+Console.WriteLine($"{p3Counter / (double)n * 100.0:f2}%");
+Console.WriteLine($"{p4Counter / (double)n * 100.0:f2}%");
+Console.WriteLine($"{p5Counter / (double)n * 100.0:f2}%");
