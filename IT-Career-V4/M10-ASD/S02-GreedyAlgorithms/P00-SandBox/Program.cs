fd5069102fff61736876576362400cd045d8c154
Alish Alishov <a.alishov@live.com>
2022-09-04 13:05:06 +0300

    ASD Exercise backpack

---
+++ IT-Career-V4/M10-ASD/S02-GreedyAlgorithms/P00-SandBox/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P00_SandBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            var subsets = CreateSubsets(arr);
+
+            foreach (var set in subsets)
+            {
+                foreach (var item in set)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static List<T[]> CreateSubsets<T>(T[] originalArray)
+        {
+            List<T[]> subsets = new List<T[]>();
+
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                int subsetCount = subsets.Count;
+                subsets.Add(new T[] { originalArray[i] });
+
+                for (int j = 0; j < subsetCount; j++)
+                {
+                    T[] newSubset = new T[subsets[j].Length + 1];
+                    subsets[j].CopyTo(newSubset, 0);
+                    newSubset[newSubset.Length - 1] = originalArray[i];
+                    subsets.Add(newSubset);
+                }
+            }
+            return subsets;
+        }
+    }
+}
