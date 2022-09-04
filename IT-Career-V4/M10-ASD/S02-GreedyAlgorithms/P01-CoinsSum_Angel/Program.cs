1d583c8f66cfcda769887aa7680018e5947ff43a
Alish Alishov <a.alishov@live.com>
2022-09-04 11:48:33 +0300

    ASD - Greedy Algorithms Exercise

---
+++ IT-Career-V4/M10-ASD/S02-GreedyAlgorithms/P01-CoinsSum_Angel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        int sum = 48;
+        int[] coins = new int[] { 25, 25, 25, 10, 10, 10, 5, 5, 5, 1, 1, 1 };
+        List<int> chosencoins = new List<int>();
+        foreach (var item in coins)
+        {
+            if (sum - item >= 0)
+            {
+                sum = sum - item;
+                chosencoins.Add(item);
+            }
+        }
+        Console.WriteLine(String.Join(", ", chosencoins));
+    }
+}
