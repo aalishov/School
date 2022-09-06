39e41626a166674caa51213720fe7ac38acfa90a
Alish Alishov <a.alishov@live.com>
2022-09-06 12:25:46 +0300

    Exercise permutation

---
+++ IT-Career-V4/M10-ASD/S01-Intro/Basketball/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basketball
+{
+    public class Team
+    {
+        private readonly List<Player> players;
+        public Team(string name, int openPositions, char group)
+        {
+            this.Name = name;
+            this.OpenPositions = openPositions;
+            this.Group = group;
+        }
+        public string Name { get; set; }
+        public int OpenPositions { get; set; }
+        public char Group { get; set; }
+        public int Count { get => players.Count; }
+    }
+}
