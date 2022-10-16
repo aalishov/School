b9bdb3b2b57b32c861e87e7c0635d5c3028e14ad
Alish Alishov <a.alishov@live.com>
2022-10-16 13:18:36 +0300

    Exercise IT Career V5

---
+++ IT-Career-V4/M10-ASD/S00-Intro/P04-Trees/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        Tree_02();
+    }
+    static void Tree_02()
+    {
+        Tree<string> tree = new Tree<string>
+            (
+                "50",
+                new Tree<string>
+                (
+                    "L",
+                    new Tree<string>
+                    (
+                        "^",
+                        new Tree<string>("1"),
+                        new Tree<string>("2")
+                    ),
+                    new Tree<string>("?")
+                )
+            );
+        tree.Print();
+        List<string> elements = tree.OrderDFS().ToList();
+        Console.WriteLine("DFS =>" + string.Join(", ", elements));
+    }
+    static void Tree_01()
+    {
+        Tree<int> tree = new Tree<int>
+            (
+                50,
+                new Tree<int>
+                (
+                    30
+                )
+            );
+        tree.Print();
+    }
+}
+
---
+++ IT-Career-V4/M10-ASD/S00-Intro/P04-Trees/Tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    // Стойност на възела
+    private T value;
+
+    // Деца на възела
+    private IList<Tree<T>> children;
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.value = value;
+        this.children = children;
+    }
+    public void Print(int indent = 0)
+    {
+        Console.Write(new string(' ', 2 * indent));
+        Console.WriteLine(this.value);
+        foreach (var child in this.children)
+        {
+            child.Print(indent + 1);
+        }
+    }
+
+    // Ред на посещаване на възлите от дървото
+    public IEnumerable<T> OrderDFS()
+    {
+        List<T> order = new List<T>();
+        this.DFS(this, order);
+        return order;
+    }
+
+    private void DFS(Tree<T> tree, List<T> order)
+    {
+        foreach (var child in tree.children)
+        {
+            this.DFS(child, order);
+        }
+        order.Add(tree.value);
+    }
+}
