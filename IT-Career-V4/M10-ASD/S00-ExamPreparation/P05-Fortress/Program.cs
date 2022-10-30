ba8c1a0127ed0bfbed08130e0cf7fb0e896bb520
Alish Alishov <a.alishov@live.com>
2022-10-30 11:35:04 +0200

    Exam preparations

---
+++ IT-Career-V4/M10-ASD/S00-ExamPreparation/P05-Fortress/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int midCol = n / 2;
+        int mid = (2 * n) - 4 - (2 * (n / 2));
+        int space = 2 * n - 2;
+
+        //First row
+        string colString = Col('/', '^', midCol, '\\');
+        string midString = $"{new string('_', mid)}";
+        Console.WriteLine($"{colString}{midString}{colString}");
+
+        //Mid rows
+        for (int i = 1; i <= n - 3; i++)
+        {
+            Console.WriteLine($"|{new string(' ', space)}|");
+        }
+
+        Console.WriteLine($"|{new string(' ', midCol + 1)}{midString}{new string(' ', midCol + 1)}|");
+        //Last row
+        string lastColString= Col('\\', '_', midCol, '/');
+        Console.WriteLine($"{lastColString}{new string(' ', mid)}{lastColString}");
+    }
+
+    public static string Col(char left, char mid, int midCount, char right)
+    {
+        return $"{left}{new string(mid, midCount)}{right}";
+    }
+}
---
+++ IT-Career-V4/M10-ASD/S00-ExamPreparation/P06-PrimeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int counter = 0;
+        int i = 0;
+        while (counter!=n)
+        {
+            i++;
+            if (IsPrime(i))
+            {
+                counter++;
+                Console.WriteLine(i);
+            }
+        }
+    }
+    public static bool IsPrime(long number)
+    {
+        if (number <= 1)
+            return false;
+        else if (number % 2 == 0)
+            return number == 2;
+
+        long N = (long)(Math.Sqrt(number) + 0.5);
+
+        for (int i = 3; i <= N; i += 2)
+            if (number % i == 0)
+                return false;
+
+        return true;
+    }
+}
