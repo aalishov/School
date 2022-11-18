de9174772355fe596900af6a21ffef918e322c88
Alish Alishov <a.alishov@live.com>
2022-11-18 11:19:08 +0200

    Exercise  10b

---
+++ School-2022_2023/10_B/S03-List/P02-RemoveNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        List<int> nums = Console.ReadLine()
+            .Split(" ")
+            .Select(int.Parse)
+            .ToList();
+        int remove = int.Parse(Console.ReadLine());
+
+        while (nums.Any(x => x == remove))
+        {
+            nums.Remove(remove);
+        }
+        //for (int i = 0; i < nums.Count; i++)
+        //{
+        //    if (nums[i]==remove)
+        //    {
+        //        nums.Remove(remove);
+        //        i--;
+        //    }
+        //}
+
+        Console.WriteLine(String.Join(", ", nums));
+    }
+}
+
---
+++ School-2022_2023/10_B/S03-List/P04-ReverseNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> names = Console.ReadLine()
+            .Split(", ")
+            .ToList();
+        for (int i = 0; i < names.Count; i++)
+        {
+            string[] name = names[i].Split(" ");
+            Array.Reverse(name);
+            //name = name.Reverse().ToArray();
+            Console.WriteLine(string.Join(" ", name));
+        }
+    }
+}
+
