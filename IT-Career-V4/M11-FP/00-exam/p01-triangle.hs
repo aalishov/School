8a1430c1f98ec67f6ff93d8f5a37b95523ecda09
Alish Alishov <a.alishov@live.com>
2022-11-20 09:46:08 +0200

    Update FP V3

--- IT-Career-V4/M11-FP/00-exam/p01-triangle.hs
+++ IT-Career-V4/M11-FP/00-exam/p01-triangle.hs
@@ -3,10 +3,12 @@ repeatString str n =
     if n==0 then ""
     else str ++ repeatString str (n-1)
 
+printTriangle :: (Eq t, Num t) => [Char] -> [Char] -> t -> [Char]
 printTriangle result c count =
     if count == 1 then c
     else
       result ++ repeatString c count ++  "\n" ++  printTriangle result c (count-1) 
 
+main :: IO ()
 main = do
     putStrLn  (printTriangle "" "!" 10)
