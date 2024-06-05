e4e8572a55b6340f96350161b353420429a9d9f9
Alish Alishov <a.alishov@live.com>
2024-06-05 09:28:55 +0300

    Add project binary search

---
+++ School-2023-2024/11_B/M02-ADS/S20-SortingAndSearchingAlgorithms/P04-BinarySearch/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace P04_BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 25, 50, -14, 8, 0, 15, 4, 32, -1, 15 };
+            Array.Sort(nums);
+
+            int result = BinarySearch.Binary(nums, 15);
+
+            Console.WriteLine(result);
+        }
+
+
+    }
+
+    public class BinarySearch
+    {
+        // BinarySearch = O(log(n))
+        public static int Binary<T>(T[] elements, T key) where T : IComparable
+        {
+            int start = 0, end = elements.Length - 1;
+            while (end >= start)
+            {
+                // middle
+                int mid = (start + end) / 2;
+
+                // compare
+                if (elements[mid].CompareTo(key) > 0)
+                {
+                    // (mid > key) => key must be on left 
+                    end = mid - 1;
+                }
+                else if (elements[mid].CompareTo(key) < 0)
+                {
+                    // (mid < key) => key must be on right 
+                    start = mid + 1;
+                }
+                else
+                {
+                    // (mid == key) => found
+                    return mid;
+                }
+            }
+            // not found
+            return -1;
+        }
+    }
+
+}
