fa1a6f6cec21722735bedd9af1a29c82d9797f65
Alish Alishov <a.alishov@live.com>
2024-10-06 09:41:08 +0300

    Exam preparation.

---
+++ IT-Career-V6/M10-ASD/ExamPreparation/P01-Permutations/Program.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+public class Program
+{
+    static List<int[]> matrix = new List<int[]>();
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] nums = Enumerable.Range(1, n).ToArray();
+
+        PermuteNoRepeat(nums, 0);
+
+        foreach (var row in matrix)
+        {
+            Console.WriteLine(string.Join(", ", row));
+        }
+    }
+
+    public static void PermuteNoRepeat<T>(T[] array, int index)
+    {
+        T[] vector = new T[array.Length];
+        bool[] used = new bool[array.Length];
+        PermuteNoRepeatAlgo(array, vector, used, index);
+    }
+
+    private static void PermuteNoRepeatAlgo<T>(T[] array, T[] vector, bool[] used, int index)
+    {
+        if (index >= vector.Length)
+        {
+            matrix.Add((int[])vector.Clone());
+            //Console.WriteLine(string.Join(" ", vector));
+        }
+        else
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    vector[index] = array[i];
+                    PermuteNoRepeatAlgo(array, vector, used, index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
+
---
+++ IT-Career-V6/M10-ASD/ExamPreparation/P02-Permutations/Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+public class Program
+{
+    static List<string> matrix = new List<string>();
+    static List<string> result = new List<string>();
+    static void Main(string[] args)
+    {
+        char[] input = string.Join("", Console.ReadLine().Split(" ")).ToCharArray();
+
+        PermuteNoRepeat(input, 0);
+
+
+        while (true)
+        {
+            string cmd = Console.ReadLine();
+            if (cmd == "end") { break; }
+            if (matrix.Contains(cmd)) 
+            { 
+                result.Add(cmd);
+            }
+        }
+
+        foreach (var word in result)
+        {
+            Console.WriteLine(string.Join(" ", word.ToCharArray()));
+        }
+    }
+
+    public static void PermuteNoRepeat<T>(T[] array, int index)
+    {
+        T[] vector = new T[array.Length];
+        bool[] used = new bool[array.Length];
+        PermuteNoRepeatAlgo(array, vector, used, index);
+    }
+
+    private static void PermuteNoRepeatAlgo<T>(T[] array, T[] vector, bool[] used, int index)
+    {
+        if (index >= vector.Length)
+        {
+            matrix.Add(string.Join("", vector.ToArray()));
+            //Console.WriteLine(string.Join(" ", vector));
+        }
+        else
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    vector[index] = array[i];
+                    PermuteNoRepeatAlgo(array, vector, used, index + 1);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+}
+
