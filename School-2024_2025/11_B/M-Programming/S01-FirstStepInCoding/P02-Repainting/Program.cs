26895ab48b42d7c8b2d0eacddd83b09871448f6f
Alish Alishov <a.alishov@live.com>
2024-10-16 13:39:18 +0300

    Exercise conditional statements

---
+++ School-2024_2025/11_B/M-Programming/S01-FirstStepInCoding/P02-Repainting/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        // Define const
+        const double PriceNylon = 1.4;
+        const double PricePaint = 14.5;
+        const double PriceDiluent = 5.00;
+        const double BagsPrice = 0.40;
+
+        // Input
+        int nylon = int.Parse(Console.ReadLine());
+        int paint = int.Parse(Console.ReadLine());
+        int diluent = int.Parse(Console.ReadLine());
+        int hours = int.Parse(Console.ReadLine());
+
+
+        double totalNylom = (nylon + 2) * PriceNylon;
+        double totalPaint = paint * 1.1 * PricePaint;
+        double totalDiluent = diluent * PriceDiluent;
+
+        double totalMaterials = totalNylom + totalPaint + totalDiluent + BagsPrice;
+
+        double workerPrice = totalMaterials * 0.3;
+        double totalWorker = hours * workerPrice;
+
+        double total = totalMaterials + totalWorker;
+        Console.WriteLine(total);
+    }
+}
+
---
+++ School-2024_2025/11_B/M-Programming/S02-ConditionalStatements/P01-GodzillaVsKong/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        double bFilm = double.Parse(Console.ReadLine());
+        int ststisti = int.Parse(Console.ReadLine());
+        double cenaNaObl = double.Parse(Console.ReadLine());
+        double dekor = bFilm / 10;
+
+
+        if (ststisti > 150)
+        {
+            cenaNaObl = cenaNaObl * 0.9;
+        }
+        double nedos = Math.Abs(bFilm - (dekor + (cenaNaObl * ststisti)));
+
+        if (dekor + (cenaNaObl * ststisti) > bFilm)
+        {
+            Console.WriteLine($"Not enough money!");
+            Console.WriteLine($"Wingard needs {nedos:f2} leva more.");
+        }
+        else
+        {
+            Console.WriteLine($"Action!");
+            Console.WriteLine($"Wingard starts filming with {nedos:f2} leva left.");
+        }
+
+    }
+}
+
