db844b9fb1db879d80f27779ecfbb2547e136b8e
Alish Alishov <a.alishov@live.com>
2025-03-27 13:57:10 +0200

    DZI prep 27.03.2025

---
+++ School-2024_2025/12_B/DZI Preparation/DZIPreparation/P08-August2023_Zad25/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+		try
+		{
+            string input = Console.ReadLine();
+            string reversed = new string(input.Reverse().ToArray());
+
+            int num = 0;
+            bool isNumber = int.TryParse(input, out num);
+            if (num < 0 || !isNumber) 
+            { 
+                throw new ArgumentException(); 
+            }
+
+            if (input.CompareTo(reversed) == 0 && isNumber)
+            {
+                Console.WriteLine($"{input} is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is NOT a palindrome");
+            }
+        }
+		catch (Exception)
+		{
+            Console.WriteLine("Incorrectly entered number");
+		}
+
+    }
+}
---
+++ School-2024_2025/12_B/DZI Preparation/DZIPreparation/P09-August2023_Zad26/Program.cs
@@ -0,0 +1,56 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        PrintOutput();
+    }
+
+    private static void PrintOutput()
+    {
+        try
+        {
+            List<double> points = ReadPoints();
+
+            Console.WriteLine($"minimal difference - {МinDpoints(points)} p.");
+            Console.WriteLine(string.Join(", ", points.OrderByDescending(x => x)));
+            Console.WriteLine("laureates - " + Laureates(points));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static int Laureates(List<double> points)
+    {
+        return points.Count(x => x >= points.Distinct().OrderByDescending(z => z).Skip(2).Take(1).First());
+    }
+
+    private static double МinDpoints(List<double> points)
+    {
+        List<double> unique = points.Distinct().ToList();
+        double minValue = double.MaxValue;
+        for (int i = 0; i < unique.Count - 1; i++)
+        {
+            if (unique[i + 1] - unique[i] < minValue)
+            {
+                minValue = unique[i + 1] - unique[i];
+            }
+        }
+        return Math.Round(minValue, 3);
+    }
+
+    private static List<double> ReadPoints()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        List<double> list = new List<double>();
+
+        for (int i = 0; i < n; i++)
+        {
+            list.Add(double.Parse(Console.ReadLine()));
+        }
+
+        return list.Where(x => x > 0).OrderBy(x => x).ToList();
+    }
+}
